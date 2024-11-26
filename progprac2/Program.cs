0c45eda62f0b4c3d6d94b0743290941687de108a
belzimbub <147517036+belzimbub@users.noreply.github.com>
2024-11-26 17:33:36 +0700

    Update Program.cs

--- progprac2/Program.cs
+++ progprac2/Program.cs
@@ -54,6 +54,50 @@ namespace Prag
                 Console.WriteLine();
             }
         }
+        public static void fourthTask()
+{
+    int n, m;
+    Console.Write("Введите количесвто строк массива M: ");
+    m = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите количесвто строк массива N: ");
+    n = Convert.ToInt32(Console.ReadLine());
+    int value = 1;
+    int[,] array = new int[m, n];
+
+    int minRow = 0, maxRow = m - 1;
+    int minCol = 0, maxCol = n - 1;
+
+    while (value <= m * n)
+    {
+        // Заполняем верхнюю строку слева направо
+        for (int i = minCol; i <= maxCol && value <= m * n; i++)
+            array[minRow, i] = value++;
+        minRow++;
+
+        // Заполняем правый столбец сверху вниз
+        for (int i = minRow; i <= maxRow && value <= m * n; i++)
+            array[i, maxCol] = value++;
+        maxCol--;
+
+        // Заполняем нижнюю строку справа налево
+        for (int i = maxCol; i >= minCol && value <= m * n; i--)
+            array[maxRow, i] = value++;
+        maxRow--;
+
+        // Заполняем левый столбец снизу вверх
+        for (int i = maxRow; i >= minRow && value <= m * n; i--)
+            array[i, minCol] = value++;
+        minCol++;
+    }
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            Console.Write(array[i, j].ToString().PadLeft(3) + " ");
+        }
+        Console.WriteLine();
+    }
+}
         static void Main()
         {
             Console.WriteLine("1. Введите размер массива:");
@@ -61,6 +105,7 @@ namespace Prag
             MassVivod(MassCreate(N));
             Console.WriteLine();
             MassSquare(N);
+            fourthTask();
         }
     }
 }
